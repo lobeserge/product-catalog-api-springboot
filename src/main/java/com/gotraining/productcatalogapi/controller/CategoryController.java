160855520fb0334243a6e50300200e07135d9489
lobeserge <lobeserge100@gmail.com>
2019-10-12 22:03:25 +0200

    added spring boot cor

--- src/main/java/com/gotraining/productcatalogapi/controller/CategoryController.java
+++ src/main/java/com/gotraining/productcatalogapi/controller/CategoryController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.gotraining.productcatalogapi.service.CategoryService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class CategoryController {
 	
 	@Autowired
